14a24360e2021c1b7a1429a8b6dbcb7e6965d737
Theerthac <127188105+Theerthac@users.noreply.github.com>
2023-10-27 13:35:02 +0530

    completed

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_project/controller/list_provider.dart';
 import 'package:simple_project/view/second_screen.dart';
 
 class Home extends StatefulWidget {
@@ -9,46 +11,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<int> numbers = [1, 2, 3];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          int last = numbers.last;
-          setState(() {
-            numbers.add(last + 1);
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: Column(
-        children: [
-          Text(
-            numbers.last.toString(),
-            style: const TextStyle(fontSize: 20),
+    return Consumer<ListProvider>(builder: (context, value, child) {
+      return Scaffold(
+          appBar: AppBar(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              value.add();
+            },
+            child: const Icon(Icons.add),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: numbers.length,
-              itemBuilder: (context, index) {
-                return Text(
-                  numbers[index].toString(),
-                  style: const TextStyle(fontSize: 20),
-                );
-              },
-            ),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>   SecondScreen(numbers:numbers,),
-                ));
-              },
-              child: const Text('NextPage'))
-        ],
-      ),
-    );
+          body: Column(
+            children: [
+              Text(
+                value.numbers.last.toString(),
+                style: const TextStyle(fontSize: 20),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: value.numbers.length,
+                  itemBuilder: (context, index) {
+                    return Text(
+                      value.numbers[index].toString(),
+                      style: const TextStyle(fontSize: 20),
+                    );
+                  },
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const SecondScreen(),
+                    ));
+                  },
+                  child: const Text('NextPage'))
+            ],
+          ));
+    });
   }
 }
--- lib/view/second_screen.dart
+++ lib/view/second_screen.dart
@@ -1,52 +1,51 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_project/controller/list_provider.dart';
 
 class SecondScreen extends StatefulWidget {
-  final List<int> numbers;
-
-  const SecondScreen({super.key, required this.numbers});
+  const SecondScreen({super.key});
 
   @override
   State<SecondScreen> createState() => _SecondScreenState();
 }
 
 class _SecondScreenState extends State<SecondScreen> {
-  
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          int last = widget.numbers.last;
-          setState(() {
-          widget.numbers.add(last + 1);
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: Column(
-        children: [
-          Text(
-            widget.numbers.last.toString(),
-            style: const TextStyle(fontSize: 20),
-          ),
-          SizedBox(
-            height: 200,
-            width: double.maxFinite,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: widget.numbers.length,
-              itemBuilder: (context, index) {
-                return Text(
-                  widget.numbers[index].toString(),
-                  style: const TextStyle(fontSize: 20),
-                );
-              },
+    return Consumer<ListProvider>(builder: (context, value, child) {
+      return Scaffold(
+        appBar: AppBar(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            value.add();
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Column(
+          children: [
+            Text(
+              value.numbers.last.toString(),
+              style: const TextStyle(fontSize: 20),
             ),
-          ),
-        ],
-      ),
-    );
+            SizedBox(
+              height: 200,
+              width: double.maxFinite,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: value.numbers.length,
+                itemBuilder: (context, index) {
+                  return Text(
+                    value.numbers[index].toString(),
+                    style: const TextStyle(fontSize: 20),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
